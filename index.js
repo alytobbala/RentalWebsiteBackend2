5e64d1b8fb4df37aab90c9c7ff29961be1de25d0
Tobbala <tobbala@emea.corpdir.net>
2025-03-22 21:43:06 +0100

    further fixes

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const cors = require("cors");
 const db = require("./models");
 const router = express.Router();
 
+
+const allowedOrigins = ['https://tobbala.netlify.app', 'http://localhost:3000'];
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+app.options('*', cors());
 
 const rentalsRouter = require("./routes/Rentals");
 app.use("/rentals", rentalsRouter);
